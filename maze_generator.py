d85370eff78c5eee55291c14d65d549c5de0621c
wawelgreg <gregloj2001@gmail.com>
2024-03-15 23:18:54 +0100

    Configure logging module

--- maze_generator.py
+++ maze_generator.py
@@ -2,4 +2,9 @@
 import logging as log
 import curses
 
-print "Hello, Vim. :)"
+log.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s', level=log.DEBUG, filename='maze_gen.log', datefmt='%Y-%m-%dT%H:%M:%S%z')
+
+log.info("----------RUNNING PROCESS----------")
+
+if __name__ == "__main__":
+	print "Hello, Vim. :)"
