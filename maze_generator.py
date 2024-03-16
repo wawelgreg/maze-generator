9fc0f0b407ac062a3c382840b92adca193906729
wawelgreg <gregloj2001@gmail.com>
2024-03-16 03:28:17 +0100

    Insert/update logging messages

--- maze_generator.py
+++ maze_generator.py
@@ -25,26 +25,35 @@ def maze_generation(rows_l, cols_l, wall):
 	# For cases if width|height too small
 	# or not odd in side length.	
 	if rows_l < 3:
+		log.error("Maze height %r too small: setting height to 3", rows_l)
                 rows_l = 3
         elif rows_l % 2 == 0:
+		log.error("Maze height %r is even: setting height to %r", rows_l, rows_l+1)
                 rows_l += 1
 
         if cols_l < 3:
+		log.error("Maze width %r too small: setting width to 3", cols_l)
                 cols_l = 3
         elif cols_l % 2 == 0:
+		log.error("Maze width %r is even: setting width to %r", cols_l, cols_l+1)
                 cols_l += 1
 
 	# Fill maze with walls
+	log.info("Filling maze matrix size: %rx%r with wall: %r", rows_l, cols_l, wall)
 	maze_matrix = []
 	for r in range(rows_l):
 		maze_matrix.append([wall for c in range(cols_l)])
+	log.info("Filled maze matrix generation success")
 
+	log.info("Starting maze generation")
 	stack = []
 	rc, cc = R_START, C_START
+	log.debug("START: Appending (r:%r, c:%r) to stack", rc, cc)
 	stack.append((rc, cc))
 
 	while len(stack) > 0:
 		r_coord, c_coord = stack.pop()	
+		log.debug("Popped (r:%r,c:%c) from stack", r_coord, c_coord)
 		maze_matrix[r_coord][c_coord] = DIG_GUY
 		
 		# Draw maze to pad
