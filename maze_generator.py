a514b3fe0c32790375ce6ee40fe028b47e0137ae
wawelgreg <gregloj2001@gmail.com>
2024-03-16 02:31:18 +0100

    feat: Handle improper maze width and height integers

--- maze_generator.py
+++ maze_generator.py
@@ -4,6 +4,9 @@ import curses
 import time
 
 WALL = '\xe2'
+DIG_GUY = 'O'
+MAZE_WIDTH = -1
+MAZE_HEIGHT = -1
 FRAME_SLEEP = 5
 
 log.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s', level=log.DEBUG, filename='maze_gen.log', datefmt='%Y-%m-%dT%H:%M:%S%z')
@@ -11,22 +14,40 @@ log.basicConfig(format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s', le
 stdscr = curses.initscr()
 pad = curses.newpad(100,100)
 
-# Temp: Testing curses
-m = []
-for r in range(5):
-	m.append([WALL for c in range(5)])
-
-def draw(matrix):
+def draw(m):
 	log.debug("Drawing to pad")
-	for ri, rm in enumerate(matrix):
-		for ci, cm in enumerate(matrix):
-			pad.addch(ri, ci, matrix[ri][ci])
+	for ri, crow in enumerate(m):
+		for ci, ccol in enumerate(crow):
+			pad.addch(ri, ci, m[ri][ci])
 
-def func(scr):
+
+def maze_generation(rows_l, cols_l, wall):
+	# For cases if width|height too small
+	# or not odd in side length.	
+	if rows_l < 3:
+                rows_l = 3
+        elif rows_l % 2 == 0:
+                rows_l += 1
+
+        if cols_l < 3:
+                cols_l = 3
+        elif cols_l % 2 == 0:
+                cols_l += 1
+
+	# Fill maze with walls
+	maze_matrix = []
+	for r in range(rows_l):
+		maze_matrix.append([wall for c in range(cols_l)])
+
+	# Draw maze to pad
 	pad.erase()
-	draw(m)
+	draw(maze_matrix)
 	pad.refresh(0,0, 0,0, curses.LINES-1,curses.COLS-1)
 	time.sleep(FRAME_SLEEP)
+
+
+def func(scr):
+	maze_generation(MAZE_HEIGHT, MAZE_WIDTH, WALL)
 
 if __name__ == "__main__":
 	log.info("<<<Running maze generator independently.>>>")
